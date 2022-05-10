eef92202022d70a62fb441750096acb196e9aff7
Sangram <sangram15.behera@gmail.com>
2022-05-11 05:08:04 +0530

    Added By Sangram

---
+++ AddressBook/AddNewContact.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        List<Contact> addressBook = new List<Contact>();
+        Contact contact = new Contact();
+        public AddressBookMain()
+        {
+            Contact contact1 = new Contact()
+            {
+                ID = "1",
+                FirstName = "sangram",
+                LastName = "behera",
+                Address = "bhadrak",
+                Email = "sangram15.behera@gmail.com",
+                PhoneNumber = "7077445649",
+                City = "banglore",
+                State = "odisha",
+                ZipCode = "756137"
+            };
+            Contact contact2 = new Contact()
+            {
+                ID = "2",
+                FirstName = "Ms",
+                LastName = "Dhoni",
+                Address = "Ranchi",
+                Email = "dhoni@gmail.com",
+                PhoneNumber = "7008015327",
+                City = "Ranchi",
+                State = "Jharkhand",
+                ZipCode = "560037"
+            };
+            addressBook.Add(contact1);
+            addressBook.Add(contact2);
+        }
+        public void CreateContact()
+        {
+            Console.WriteLine("Enter Your ID: ");
+            contact.ID = Console.ReadLine();
+            Console.WriteLine("Enter Your First: ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Your Last Name: ");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Your Address: ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter Your Email Id: ");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Enter Your PhoneNumber: ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Your City: ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter Your State: ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter Your Zip Code: ");
+            contact.ZipCode = Console.ReadLine();
+            addressBook.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressBook)
+            {
+                Console.WriteLine(contact.ID + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.Email + "\n" + contact.PhoneNumber + "\n" + contact.City + "\n" + contact
+                    .State + "\n" + contact.ZipCode);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
